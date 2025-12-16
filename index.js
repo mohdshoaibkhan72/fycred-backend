414fa5738e4c685de5105978525a86c9b851a468
mohdshoaibkhan72 <mohdshoaibkhan7211@gmail.com>
2025-12-16 18:39:22 +0530

    updates

--- index.js
+++ index.js
@@ -21,7 +21,7 @@ const app = express();
 const PORT = process.env.PORT || 5000;
 
 // Middleware
-app.use(cors());
+app.use(cors({ origin: '*' }));
 app.use(express.json());
 
 // Database Connection
